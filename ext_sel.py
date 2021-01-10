a4a496546a651d99fcdd79acff0525a72a0cef12
halfbrained <zulufaka.ms@gmail.com>
2021-01-10 16:49:12 +0200

    added menu entry

--- ext_sel.py
+++ ext_sel.py
@@ -57,41 +57,54 @@ class Command:
       """ store modified selection to be applied in on_caret()
       """
       if state == 'sL'  or  state == 'Ls': # only shift and left mouse are pressed 
-        lex = ed_self.get_prop(PROP_LEXER_CARET) 
-        comment_str = lexer_proc(LEXER_GET_PROP, lex)["c_line"]  if lex else  ''
+        self.calc_selections()
+        
+        
+    def menu_entry(self):
+      """ if no selection and no stored: calculate selections  and  apply once
+      """
+      if not self.triple_sel:
+        self.calc_selections()
+      self.apply_selection(ignore_time=True)
+      
+      
+    def calc_selections(self):
+      lex = ed.get_prop(PROP_LEXER_CARET) 
+      comment_str = lexer_proc(LEXER_GET_PROP, lex)["c_line"]  
          
-        include_chars = INCLUDE_CHARS.get(lex, INCLUDE_CHARS['default']) 
-        incl_pattern = f'^[\w'+include_chars+']*'
+      include_chars = INCLUDE_CHARS.get(lex, INCLUDE_CHARS['default']) 
+      incl_pattern = f'^[\w'+include_chars+']*'
       
-        caret_x, caret_y = ed_self.get_carets()[0][:2]
+      caret_x, caret_y = ed.get_carets()[0][:2]
       
-        l = 0
-        textl = ed_self.get_text_substr( max(0, caret_x-255), caret_y,   caret_x, caret_y )
-        lmatch = re.search(incl_pattern, textl[::-1])
-        if lmatch:
-          l = lmatch.end()
+      l = 0
+      textl = ed.get_text_substr( max(0, caret_x-255), caret_y,   caret_x, caret_y )
+      lmatch = re.search(incl_pattern, textl[::-1])
+      if lmatch:
+        l = lmatch.end()
 
-        r = 0
-        textr = ed_self.get_text_substr(caret_x, caret_y, caret_x+255, caret_y)
-        if comment_str:
-          textr = textr.split(comment_str, 1)[0]  # stop selection at comment string
-        rmatch = re.search(incl_pattern, textr)
-        if rmatch:
-          r = rmatch.end()
+      r = 0
+      textr = ed.get_text_substr(caret_x, caret_y, caret_x+255, caret_y)
+      textr = textr.split(comment_str, 1)[0]  # stop selection at comment string
+      rmatch = re.search(incl_pattern, textr)
+      if rmatch:
+        r = rmatch.end()
         
-        open_chars = OPEN_CHARS.get(lex, OPEN_CHARS['default'])
-        close_chars = CLOSE_CHARS.get(lex, CLOSE_CHARS['default'])
+      open_chars = OPEN_CHARS.get(lex, OPEN_CHARS['default'])
+      close_chars = CLOSE_CHARS.get(lex, CLOSE_CHARS['default'])
           
-        if l > 0 or r > 0:
-          x1 = caret_x-l
-          x2 = caret_x+r
-          self.to_sel = (x1,caret_y, x2,caret_y)
+      if l > 0 or r > 0:
+        x1 = caret_x-l
+        x2 = caret_x+r
+        # store expanded double click selection to apply later
+        self.to_sel = (x1,caret_y, x2,caret_y)  
         
-        clicked_bracket = textr and (textr[0] in open_chars  or  textr[0] in close_chars)
-        if l > 0  or  r > 0  or  clicked_bracket: 
-          expr_l = self.get_expression_sel(textl, lex, reverse=True)
-          expr_r = self.get_expression_sel(textr, lex)
-          self.triple_sel = (time.time()+click_time,  caret_x-len(expr_l), caret_y,  caret_x+len(expr_r), caret_y)
+      clicked_bracket = textr and (textr[0] in open_chars  or  textr[0] in close_chars)
+      if l > 0  or  r > 0  or  clicked_bracket: 
+        expr_l = self.get_expression_sel(textl, lex, reverse=True)
+        expr_r = self.get_expression_sel(textr, lex)
+        # store expanded triple click selection to apply later (-if third click comes)
+        self.triple_sel = [time.time()+click_time,  caret_x-len(expr_l), caret_y,  caret_x+len(expr_r), caret_y]
           
           
     def get_expression_sel(self, s, lex, reverse=False): 
@@ -118,7 +131,7 @@ class Command:
           else:
             lvls[open_char] -= 1 # '..(..)'
         
-        elif ch in stop_ext  and  all(level == 0 for level in lvls.values()): # stop char is not inside any brackets - stop  
+        elif ch in stop_ext  and  all(level == 0 for level in lvls.values()): # stop char is not inside any brackets - stop
           s = s[:i].rstrip()
           break
       
@@ -135,18 +148,24 @@ class Command:
       if self.skip_caret:
         self.skip_caret = False
         return
+        
+      self.apply_selection(ed_self)
+      
 
-
+    def apply_selection(self, ed_self=None, ignore_time=False):
       if self.to_sel:
         x1,y1, x2,y2 = self.to_sel
         self.to_sel = None
         self.skip_caret = True
-        ed_self.set_sel_rect(x1, y1,  x2, y2)
+        ed.set_sel_rect(x1, y1,  x2, y2)
 
-      elif self.triple_sel  and  self.triple_sel[0] >= time.time():
-        x1,y1, x2,y2 = self.triple_sel[1:5]
+      elif self.triple_sel:
+        if self.triple_sel[0] >= time.time()  or  ignore_time:
+          x1,y1, x2,y2 = self.triple_sel[1:5]
+          self.triple_sel = None # needs to be before set_sel_rect()
+          ed.set_sel_rect(x1, y1,  x2, y2)
+          
         self.triple_sel = None
-        ed_self.set_sel_rect(x1, y1,  x2, y2)
         
         
     def load_config(self):
@@ -157,39 +176,35 @@ class Command:
       OPEN_CHARS, CLOSE_CHARS, STOP_EXT, INCLUDE_CHARS = {},{},{},{}
       
       try:
-        if not os.path.isfile(fn_config):
-          print('Extended double-click selection: Missing config file. Creating a default one.')
-          with open(fn_config, 'w') as f:
-            f.write(_config_json)
+        if os.path.isfile(fn_config):
+          with open(fn_config, 'r') as f:
+            j = json.load(f)
           
-        with open(fn_config, 'r') as f:
-          j = json.load(f)
+            # default
+            jd = j['default']
+            OPEN_CHARS    = {'default':jd['open_chars'] }
+            CLOSE_CHARS   = {'default':jd['close_chars'] }
+            INCLUDE_CHARS = {'default':jd['include_chars'] }
+            STOP_EXT      = {'default':jd['stop_ext'] }
           
-          # default
-          jd = j['default']
-          OPEN_CHARS    = {'default':jd['open_chars'] }
-          CLOSE_CHARS   = {'default':jd['close_chars'] }
-          INCLUDE_CHARS = {'default':jd['include_chars'] }
-          STOP_EXT      = {'default':jd['stop_ext'] }
-          
-          # lexers
-          for key,val in j.items():
-            if key[0] == '_' or key == 'default': 
-              continue
-              
-            for lex in key.split(','):
-              if not lex:
+            # lexers
+            for key,val in j.items():
+              if key[0] == '_' or key == 'default': 
                 continue
+              
+              for lex in key.split(','):
+                if not lex:
+                  continue
                 
-              OPEN_CHARS[lex]     = val.get('open_chars', OPEN_CHARS['default']) 
-              CLOSE_CHARS[lex]    = val.get('close_chars', CLOSE_CHARS['default']) 
-              INCLUDE_CHARS[lex]  = val.get('include_chars', INCLUDE_CHARS['default']) 
-              STOP_EXT[lex]       = val.get('stop_ext', STOP_EXT['default']) 
+                OPEN_CHARS[lex]     = val.get('open_chars', OPEN_CHARS['default']) 
+                CLOSE_CHARS[lex]    = val.get('close_chars', CLOSE_CHARS['default']) 
+                INCLUDE_CHARS[lex]  = val.get('include_chars', INCLUDE_CHARS['default']) 
+                STOP_EXT[lex]       = val.get('stop_ext', STOP_EXT['default']) 
                 
       except Exception as e:
         OPEN_CHARS, CLOSE_CHARS, STOP_EXT, INCLUDE_CHARS = open_chars, close_chars, stop_ext, include_chars
         
         print('Extended Mouse Selection: Failed to load config, using defaults')
-        print(f' - Error:{type(e)}: {e}') # 'raise' here is not printed
+        print(e) # 'raise' here is not printed
       
 
